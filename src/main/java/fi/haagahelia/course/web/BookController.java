65d7e89ac0484dc24851c6c6ee204920a5b52239
Polianin Kirill <a1600629@myy.haaga-helia.fi>
2017-02-02 12:42:35 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -0,0 +1,20 @@
+package fi.haagahelia.course.web;
+
+
+import fi.haagahelia.course.domain.Book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+	
+@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String hello(@ModelAttribute Book book) {
+		
+		return "index";
+	}
+
+}
