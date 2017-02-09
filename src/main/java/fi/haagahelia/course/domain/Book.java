35c2778b620de9bcc4c716ed10e9f9bea67f019c
Polianin Kirill <a1600629@myy.haaga-helia.fi>
2017-02-09 18:00:28 +0300

    Adding JPA and database

--- src/main/java/fi/haagahelia/course/domain/Book.java
+++ src/main/java/fi/haagahelia/course/domain/Book.java
@@ -1,11 +1,31 @@
 package fi.haagahelia.course.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
 	private int price;
+	
+	public Book() {}
+	
+	public Book(String title, String author, int year, String isbn, int price) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -37,6 +57,8 @@ public class Book {
 		this.price = price;
 	}
 	
-	
-	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn+ ", price=" + price + "]";
+	}
 }
